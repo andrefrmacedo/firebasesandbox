09380d0898212adc8338ba8ed69b0f6801021809
Andre Macedo <andrefrmacedo@gmail.com>
2015-07-21 15:33:31 +0100

    Auth protected data fetch fom firebase

--- app/routes/mymessages.js
+++ app/routes/mymessages.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(){
-		return this.store.query('message', { uid: this.get('session.currentUser.id') });
+		return this.store.query('message', { orderBy: 'uid',equalTo: this.get('session.currentUser.id') });
 	}
 });
