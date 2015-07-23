4b65376ecef5308d226340c5e824a435762a108d
Andre Macedo <andrefrmacedo@gmail.com>
2015-07-23 16:31:10 +0100

    Second attempt of making charts
    Charts were not updating upon data changes (Temporary fix: toggle 'display' everytime something in chart changes

--- app/controllers/charts.js
+++ app/controllers/charts.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 
 export default Ember.Controller.extend({
+	display: true,
+	type: 'Line',
 	dataBar: {
 		labels: ["January", "February", "March", "April", "May", "June", "July"],
 		datasets: [{
@@ -18,22 +20,49 @@ export default Ember.Controller.extend({
 			highlightFill: "rgba(100,220,100,0.75)",
 			highlightStroke: "rgba(151,187,205,1)",
 			data: [28, 48, 40, 19, 86, 27, 90]
-		}]
+		}],
+
 	},
 	dataPie: [{
-        value: 300,
+        value: 66,
         color:"#F7464A",
         highlight: "#FF5A5E",
         label: "Red"
     }, {
-        value: 50,
+        value: 11,
         color: "#46BFBD",
         highlight: "#5AD3D1",
         label: "Green"
     }, {
-        value: 100,
+        value: 23,
         color: "#FDB45C",
         highlight: "#FFC870",
         label: "Yellow"
-    }]
+    }],
+
+    actions:{
+    	addData: function(){
+    		console.log(this.dataBar.labels);
+    		this.dataBar.labels.push('New Month');
+    		this.dataBar.datasets[0].data.push(25);
+    		this.dataBar.datasets[1].data.push(50);
+    		if(this.display){
+    			this.set('display',false);
+    		}
+    		else{
+    			this.set('display',true);
+    		}	
+    	},
+    	changeGraph: function(){
+    		if(this.display){
+    			this.set('type','Bar');
+    			this.set('display',false);
+    		}
+    		else{
+    			this.set('type','Line');
+    			this.set('display',true);
+    		}
+    	}
+    }
+
 });
